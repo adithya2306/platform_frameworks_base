8b6cdbc670b126a98c400ae0f4d24fbea91640f0
Jesse Dai <jessedai@google.com>
2025-03-26 23:15:06 +0000

    Convert KeyguardTouchHandlingViewModel to Activatable
    Following go/sysui-arch:summer-24 to convert the view model into
    an Activatable so the lifecycle of coroutines can be automatically
    handled in views or composables.
    
    Bug: 406586544
    Test: atest SystemUiRoboTests
    Test: manual - verify tap to wake with AOD on/off
    Test: manual - verify long press launches default settings menu and
    touching outside areas will dismiss the settings menu
    Test: manual - verify double tap in empty space goes to sleep
    Flag: EXEMPT refactor
    
    Change-Id: Ic3314a1e4818379a3f7e306b49855afb35837423

--- packages/SystemUI/multivalentTests/src/com/android/systemui/shade/NotificationPanelViewControllerBaseTest.java
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/shade/NotificationPanelViewControllerBaseTest.java
@@ -261,7 +261,7 @@ public class NotificationPanelViewControllerBaseTest extends SysuiTestCase {
     @Mock protected DreamingToLockscreenTransitionViewModel
             mDreamingToLockscreenTransitionViewModel;
     @Mock protected KeyguardTransitionInteractor mKeyguardTransitionInteractor;
-    @Mock protected KeyguardTouchHandlingViewModel mKeyuardTouchHandlingViewModel;
+    @Mock protected KeyguardTouchHandlingViewModel.Factory mKeyguardTouchHandlingViewModelFactory;
     @Mock protected WallpaperFocalAreaViewModel mWallpaperFocalAreaViewModel;
     @Mock protected AlternateBouncerInteractor mAlternateBouncerInteractor;
     @Mock protected MotionEvent mDownMotionEvent;
@@ -566,7 +566,7 @@ public class NotificationPanelViewControllerBaseTest extends SysuiTestCase {
                 mMainDispatcher,
                 mKeyguardTransitionInteractor,
                 mDumpManager,
-                mKeyuardTouchHandlingViewModel,
+                mKeyguardTouchHandlingViewModelFactory,
                 mWallpaperFocalAreaViewModel,
                 mKeyguardInteractor,
                 mActivityStarter,
