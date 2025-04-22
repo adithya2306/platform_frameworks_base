e66c623517fa64cdec747e013dffcf2e495ed043
Rahul Banerjee <rahulbanerjee@google.com>
2025-04-21 17:27:16 -0700

    Decouple shade scrim from Dual Shade color
    Bug: 406043695
    Test: Manual (build, flash, verify with lt/dt)
    Flag: com.android.systemui.notification_shade_blur
    Change-Id: Ia923be1cc1fc0be5350982283312c8a1c57c3715

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -1654,7 +1654,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     }
 
     private int getShadePanelColor() {
-        return ShadeColors.shadePanel(mContext, isBlurCurrentlySupported());
+        return ShadeColors.shadePanel(mContext, isBlurCurrentlySupported(), true);
     }
 
     private void onThemeChanged() {
